89e0cfb1f62aa916fbb5fa0a74215863cb3c31de
dino460 <raphael.z.cali@gmail.com>
2023-09-17 10:27:00 -0300

    Minor Camera tweaks

--- scripts/camera_controller.gd
+++ scripts/camera_controller.gd
@@ -8,15 +8,15 @@ var is_rotating: bool = false
 var target_rotation: float = 0.0
 
 func _ready() -> void:
-    self.rotation.y = 0.0
+	self.rotation.y = 0.0
 
 func _process(delta: float) -> void:
-    if Input.is_action_just_pressed("rotate_camera_clockwise"):
-        target_rotation += deg_to_rad(rotation_angle)
-    elif Input.is_action_just_pressed("rotate_camera_anticlockwise"):
-        target_rotation -= deg_to_rad(rotation_angle)
-        
-    if not is_equal_approx(self.rotation.y, target_rotation):
-        self.rotation.y = lerp_angle(self.rotation.y, target_rotation, delta * rotation_speed)
-    elif is_equal_approx(self.rotation.y, target_rotation):
-        self.rotation.y = target_rotation
+	if Input.is_action_just_pressed("rotate_camera_clockwise"):
+		target_rotation += deg_to_rad(rotation_angle)
+	elif Input.is_action_just_pressed("rotate_camera_anticlockwise"):
+		target_rotation -= deg_to_rad(rotation_angle)
+		
+	if not is_equal_approx(self.rotation.y, target_rotation):
+		self.rotation.y = lerp_angle(self.rotation.y, target_rotation, delta * rotation_speed)
+	else
+		self.rotation.y = target_rotation
