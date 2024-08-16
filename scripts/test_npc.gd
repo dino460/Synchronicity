b523b88d0ee4f70940bb3670d9b209181928d877
dino460 <raphael.z.cali@gmail.com>
2024-08-15 21:48:17 -0300

    Upgrade to Godot 4.3, fixed code from 4.2.2, added static navigation obstacles

--- scripts/test_npc.gd
+++ scripts/test_npc.gd
@@ -1,43 +1,38 @@
 extends CharacterBody3D
 
-var movement_speed: float = 3.5
-var movement_target_position: Vector3 = Vector3(-3.0, 0.0, 2.0)
+@export var movement_speed: float = 4.0
+@export var movement_targets: Array[Node3D]
 
-@export var targets: Array[Node3D]
-@export var current_target_index : int = 0
+var current_index : int = 0
+var first_iteration : bool = true
 
 @onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
-@onready var collisiton_shape: CollisionShape3D = $CollisionShape3D
 
 func _ready():
-	# These values need to be adjusted for the actor's speed
-	# and the navigation layout.
-	navigation_agent.path_desired_distance = 0.5
-	navigation_agent.target_desired_distance = 0.5
-
 	# Make sure to not await during _ready.
-	if not targets.is_empty():
-		call_deferred("actor_setup")
+	call_deferred("actor_setup")
 
 func actor_setup():
+	navigation_agent.debug_enabled = true
 	# Wait for the first physics frame so the NavigationServer can sync.
 	await get_tree().physics_frame
-	print(targets[current_target_index].position)
+
 	# Now that the navigation map is no longer empty, set the movement target.
-	set_movement_target(targets[current_target_index].position)
+	set_movement_target(movement_targets[current_index].position)
 
 func set_movement_target(movement_target: Vector3):
+	first_iteration = false
 	navigation_agent.set_target_position(movement_target)
 
-func _physics_process(_delta):
-	if navigation_agent.is_navigation_finished():
-		current_target_index += 1
-		
-		if current_target_index > targets.size() - 1:
-			current_target_index = 0
+func _physics_process(delta):
+	print(navigation_agent.target_desired_distance, "\n", navigation_agent.distance_to_target())
+
+	if navigation_agent.is_navigation_finished() and not first_iteration:
+		current_index += 1
+		if current_index >= movement_targets.size():
+			current_index = 0
 			
-		set_movement_target(targets[current_target_index].position)
-		
+		set_movement_target(movement_targets[current_index].position)
 		return
 
 	var current_agent_position: Vector3 = global_position
