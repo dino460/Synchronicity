2bf14ebd4b436df543a0646c905cf8aa6709c0b6
dino460 <raphael.z.cali@gmail.com>
2024-08-25 14:03:47 -0300

    Day/Night cycle + better sky and light

--- scripts/npc/scheduler.gd
+++ scripts/npc/scheduler.gd
@@ -1,9 +1,11 @@
 extends Timer
 
-@export var full_day_time : int = 600
+@export var full_day_time : float = 600.0
 @export var sun_up_time_proportion : float = 0.5
 
 @export var is_sun_up : bool = false
+
+@export var sun : DirectionalLight3D
 
 
 func _ready() -> void:
@@ -12,9 +14,17 @@ func _ready() -> void:
 	start()
 
 func _process(_delta: float) -> void:
-	if time_left <= (full_day_time * sun_up_time_proportion):
-		is_sun_up = false
+	is_sun_up = false if time_left <= (full_day_time * sun_up_time_proportion) else true
+	
+	rotate_sun()
 	
 	if is_stopped():
-		is_sun_up = true
 		start()
+	
+func rotate_sun():
+	if sun != null:
+		sun.rotation_degrees = Vector3(
+			(time_left / full_day_time) * 360.0, 
+			sun.rotation_degrees.y, 
+			sun.rotation_degrees.z
+		)
