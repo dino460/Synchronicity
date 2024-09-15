516864bccae1c082abf63ca698b115f901abfe95
dino460 <raphael.z.cali@gmail.com>
2024-09-15 10:13:12 -0300

    [0.1.0.1a] FIX :: Pixelation by Window Size Fixed Pixelation apply code so it changes value on window size change Body: (+) Connected the pixelation value calculation and apply to the "size_changed signal of the viewport from: [0.1.0.0a]

--- scripts/handle_pixelation.gd
+++ scripts/handle_pixelation.gd
@@ -1,13 +1,14 @@
 extends SubViewportContainer
 
 func _ready() -> void:
-	change_pixelation_by_resolution()
+	get_viewport().connect("size_changed", _change_pixelation_by_resolution)
+	_change_pixelation_by_resolution()
 	pass
 
-func change_pixelation_by_resolution():
+func _change_pixelation_by_resolution():
 	var current_res = mini(DisplayServer.window_get_size().x, DisplayServer.window_get_size().y)
-	
+
 	stretch_shrink = max(5, 5 * current_res / 1080)
-	
+
 	print(current_res, " ", stretch_shrink)
 	pass
