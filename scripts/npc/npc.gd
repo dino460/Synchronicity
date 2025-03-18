91f57a0bb781f85e157f638e4ecd9aa115746ce9
dino460 <raphael.z.cali@gmail.com>
2025-03-18 08:41:19 -0300

    [0.3.9.0a] FEAT :: NPC Animations Added body and enabled walking and idling animations for NPCs Body: (+) Pivoting mesh repurposed from player model (+) Code to handle playing animation for NPC when walking and idling from: [0.3.8.1a]

--- scripts/npc/npc.gd
+++ scripts/npc/npc.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 class_name NPC
 
-const base_move_speed : float = 8.0
+const base_move_speed : float = 3.3
 const base_run_multiplier : float = 2.3
 
 enum State {DOING_STUFF, MOVING_ABOUT, SLEEPING, DEAD}
@@ -40,6 +40,7 @@ var current_location : Landmark
 var last_location : Landmark
 
 var is_running : bool = false
+var direction : Vector3 = Vector3.ZERO
 var navigation_enabled : bool = false
 
 var is_at_home : bool = false
@@ -50,6 +51,9 @@ var sleep_amount_wanted : float = 0.0
 var sleep_counter : float = 0.0
 
 @export var stats : CharacterStats
+
+@onready var mesh_pivot_ref = $MeshPivot
+@onready var animator = $"MeshPivot/Low-Poly-Base_blend/AnimationPlayer" #! MOVE ANIMATION CODE TO DEDICATED SCRIPT
 
 # var is_doing_stuff : bool = false
 # var is_moving_about : bool = false
@@ -127,14 +131,19 @@ func _process(delta: float) -> void:
 			if not has_worked_today:
 				has_worked_today = job.has_worked_today(get_landmark_timer(job, true))
 
-func _physics_process(_delta):
-	# if current_target != null:
+func _physics_process(delta):
+	if current_target != null:
+		direction = (navigation_agent.get_next_path_position() - position).normalized()
 	# 	print_rich(">>> want go: [color=red][b] %s [/b][/color]" % current_target.landmark_name)
 	# if current_location != null:
 	# 	print_rich(">>> I am at: [color=red][b] %s [/b][/color]" % current_location.landmark_name)
+	mesh_pivot_ref.rotation.y = lerp_angle(mesh_pivot_ref.rotation.y, atan2(-direction.x, -direction.z), delta * 20.0)
 
 	if current_location != current_target:
-		position += velocity * _delta
+		position += velocity * delta
+		animator.play("walk", 0.1, 3.8, false) #! MOVE ANIMATION CODE TO DEDICATED SCRIPT
+	else:
+		animator.play("idle", 0.1, 3.0, false) #! MOVE ANIMATION CODE TO DEDICATED SCRIPT
 
 func run_pathfinding_logic():
 	if current_state == State.DEAD:
