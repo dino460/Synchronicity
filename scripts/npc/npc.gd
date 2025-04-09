8fda67c52473c9b8abddaa2143d9294fcd34f7fb
dino460 <raphael.z.cali@gmail.com>
2025-04-09 16:10:37 -0300

    [0.3.12.2a] CHORE :: Code Polishing Polished npc code in preparation for combat development Body: (*) Grouped variables in npc code (*) Simplified code (-) Removed dead code (-) Removed unecessary comments from: [0.3.12.1a]

--- scripts/npc/npc.gd
+++ scripts/npc/npc.gd
@@ -2,23 +2,34 @@ extends CharacterBody3D
 
 class_name NPC
 
-const base_move_speed : float = 3.3
+const base_move_speed     : float = 3.3
 const base_run_multiplier : float = 2.3
 
-enum State {DOING_STUFF, MOVING_ABOUT, SLEEPING, DEAD}
+signal idling
+signal walking
+signal death
+
+enum State {DOING_STUFF, MOVING_ABOUT, SLEEPING, DEAD, FIGHTING}
 var current_state : State = State.DOING_STUFF
 
-@export var npc_name : String
-@export var id : int
-var process_group : int
+@export var test_label : Label
 
-@export var timers : Dictionary
+@export_group("NPC Identity")
+@export var npc_name : String
+@export var id       : int
+var process_group    : int
+
+@export_group("NPC Scheduling")
+@export var timers    : Dictionary
 @export var scheduler : Scheduler
 
+@export_group("NPC Characteristics")
 @export var current_age        : int
 @export var cycles_to_next_age : int
-@export var personality : Personality
+@export var personality        : Personality
+@export var stats              : CharacterStats
 
+@export_group("NPC Landmarks")
 @export var job                : Job
 @export var home               : Home
 @export var visits             : Dictionary
@@ -26,39 +37,36 @@ var process_group : int
 
 @export var average_poi_distance : float
 
-@export var has_worked_today : bool = false
-var work_time_this_day : float = 0.0
+@export_group("NPC Work")
+@export var has_worked_today   : bool = false
+
+var work_time_this_day         : float = 0.0
 var moving_about_time_this_day : float = 0.0
-var awake_time_this_day : float = 0.0
+var awake_time_this_day        : float = 0.0
 
+var want_to_sleep       : bool = false
+var sleep_amount_wanted : float = 0.0
+var sleep_counter       : float = 0.0
+
+@export_group("NPC Navigation")
 @onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
-
-@export var test_label : Label
 
 var current_target   : Landmark
 var current_location : Landmark
-var last_location : Landmark
+var last_location    : Landmark
 
-var is_running : bool = false
-var direction : Vector3 = Vector3.ZERO
-var navigation_enabled : bool = false
-
+var is_at_job  : bool = false
 var is_at_home : bool = false
-var is_at_job : bool = false
 
-var want_to_sleep : bool = false
-var sleep_amount_wanted : float = 0.0
-var sleep_counter : float = 0.0
+@export_group("NPC Movement")
+var is_running         : bool = false
+var direction          : Vector3 = Vector3.ZERO
+var navigation_enabled : bool = false
+var is_resting         : bool = false
 
-@export var stats : CharacterStats
-
-var is_in_frustum : bool = true
+@export_group("NPC Rendering")
 @onready var mesh_pivot_ref = $MeshPivot
-
-signal idling
-signal walking
-signal death
-signal stop_animations
+var is_in_frustum : bool = true
 
 
 func mod_by_age() -> float:
@@ -89,17 +97,13 @@ func _ready():
 
 	if last_location == null:
 		last_location = home
-	# Make sure to not await during _ready.
-	call_deferred("actor_setup")
+
+	call_deferred("actor_setup") # Make sure to not await during _ready.
 
 func actor_setup():
 	choose_target()
-	# navigation_agent.debug_enabled = true
-	# Wait for the first physics frame so the NavigationServer can sync.
-	await get_tree().physics_frame
-
-	# Now that the navigation map is no longer empty, set the movement target.
-	set_movement_target()
+	await get_tree().physics_frame # Wait for the first physics frame so the NavigationServer can sync.
+	set_movement_target() # Now that the navigation map is no longer empty, set the movement target.
 
 func set_movement_target():
 	if current_target == null:
@@ -112,32 +116,34 @@ func _process(delta: float) -> void:
 	if want_to_sleep and current_location == home:
 		sleep_counter += delta
 		current_state = State.SLEEPING
-
 		if sleep_counter >= sleep_amount_wanted:
-			print("AWAKE | ", scheduler.get_current_time_24())
-			want_to_sleep = false
-			sleep_amount_wanted = 0.0
-			sleep_counter = 0.0
-			awake_time_this_day = 0.0
-			current_state = State.DOING_STUFF
+			reset_sleep()
+
 	elif not want_to_sleep and current_state == State.MOVING_ABOUT:
 		moving_about_time_this_day += delta
 	else:
 		awake_time_this_day += delta
 
 	for timer in timers:
-		if current_state == State.DOING_STUFF:
-			# print(timers[timer])
-			timers[timer] += delta
-			if not has_worked_today:
-				has_worked_today = job.has_worked_today(get_landmark_timer(job, true))
+		if current_state != State.DOING_STUFF:
+			continue
+		timers[timer] += delta
+		if has_worked_today:
+			continue
+		has_worked_today = job.has_worked_today(get_landmark_timer(job, true))
+
+		# if current_state == State.DOING_STUFF:
+		# 	# print(timers[timer])
+		# 	timers[timer] += delta
+		# 	if not has_worked_today:
+		# 		has_worked_today = job.has_worked_today(get_landmark_timer(job, true))
 
 func _physics_process(delta):
 	if current_target != null:
 		direction = (navigation_agent.get_next_path_position() - position).normalized()
 	mesh_pivot_ref.rotation.y = lerp_angle(mesh_pivot_ref.rotation.y, atan2(-direction.x, -direction.z), delta * 20.0)
 
-	if current_location != current_target:
+	if current_location != current_target and not is_resting:
 		position += velocity * delta
 
 	if is_in_frustum:
@@ -163,28 +169,22 @@ func run_pathfinding_logic():
 		calculate_average_poi_distance()
 		# add_visit()
 		current_location = current_target
-		# is_doing_stuff = true
-		# is_moving_about = false
 		current_state = State.DOING_STUFF
 
 		timers[current_location] = 0.0
 
 		if current_location == job:
-			# print_rich("[color=cyan]Arrived at [/color][color=red][b]%s[/b][/color] | %s" % [current_location.landmark_name, scheduler.get_current_time_24()])
 			pass
 		elif current_location == home:
 			if has_worked_today:
 				want_to_sleep = ((work_time_this_day * 0.15) + moving_about_time_this_day + (awake_time_this_day / 2.0)) / scheduler.full_day_time > personality.mind * personality.energy
 				sleep_amount_wanted = max(4.0 * scheduler.full_day_time / 24.0, min(7.0 * scheduler.full_day_time / 24.0, work_time_this_day + moving_about_time_this_day))
-				# print(sleep_amount_wanted)
-				# print(moving_about_time_this_day)
-			# print_rich("[color=cyan]Arrived at [/color][color=green][b]%s[/b][/color] | %s" % [current_location.landmark_name, scheduler.get_current_time_24()])
-		# else:
-			# print_rich("[color=cyan]Arrived at [/color][color=yellow][b]%s[/b][/color] | %s" % [current_location.landmark_name, scheduler.get_current_time_24()])
 		return
 
-	check_for_path_while_doing_stuff()
-	check_for_path_while_moving()
+	if current_state == State.DOING_STUFF:
+		check_for_path_while_doing_stuff()
+	elif current_state == State.MOVING_ABOUT:
+		check_for_path_while_moving()
 
 	var current_agent_position: Vector3 = global_position
 	var next_path_position: Vector3 = navigation_agent.get_next_path_position()
@@ -192,9 +192,6 @@ func run_pathfinding_logic():
 	velocity = current_agent_position.direction_to(next_path_position) * get_move_speed()
 
 func check_for_path_while_doing_stuff():
-	if current_state != State.DOING_STUFF:
-		return
-
 	choose_target()
 	if current_location == current_target:
 		return
@@ -202,19 +199,13 @@ func check_for_path_while_doing_stuff():
 	if current_location == job:
 		work_time_this_day = timers[current_location]
 		has_worked_today = true
-		# print(work_time_this_day * 24.0 / scheduler.full_day_time)
 
 	timers.erase(current_location)
 	set_movement_target()
 	last_location = current_location
-	# is_doing_stuff = false
-	# is_moving_about = true
 	current_state = State.MOVING_ABOUT
 
 func check_for_path_while_moving():
-	if current_state != State.MOVING_ABOUT:
-		return
-
 	choose_target()
 	if current_location == current_target:
 		return
@@ -227,11 +218,10 @@ func time_to_get_to_target() -> float:
 func choose_target():
 	if home == null or job == null:
 		return
-
-	var targets_to_choose : Dictionary
-
 	if last_location == null:
 		last_location = home
+
+	var targets_to_choose : Dictionary
 
 	var interference = 0.0
 	if has_worked_today and scheduler.get_current_time() <= (scheduler.full_day_time * home.time_want_to_arrive / 24.0):
@@ -247,24 +237,23 @@ func choose_target():
 		targets_to_choose[last_location] /= 10.0
 
 	current_target = targets_to_choose.keys()[0]
-	var text : String = ""
-	# print_rich(current_target.name, " ", targets_to_choose[targets_to_choose.keys()[0]])
-	# text += current_target.name + " : " + targets_to_choose[targets_to_choose.keys()[0]] + "\n"
 	for landmark in targets_to_choose:
-		# print_rich(landmark.landmark_name, " ", targets_to_choose[landmark])
-		text += str(landmark.landmark_name, " : ", targets_to_choose[landmark], "\n")
 		if targets_to_choose[landmark] > targets_to_choose[current_target]:
-			# print_rich("[color=red][b][i]%s :: %f[/i][/b][/color]" % [landmark.landmark_name, targets_to_choose[landmark]])
 			current_target = landmark
-
-	if test_label != null:
-		test_label.text = text
 
 func generate_interference() -> float:
 	return randf_range(0.0, 1.0) * min(0.3, personality.chaos * exp(-randf_range(0.0, 2.0)) / (sqrt(scheduler.full_day_time / 100)))
 
 func reset_has_worked_today():
 	has_worked_today = false
+
+func reset_sleep():
+	print("AWAKE | ", scheduler.get_current_time_24())
+	want_to_sleep = false
+	sleep_amount_wanted = 0.0
+	sleep_counter = 0.0
+	awake_time_this_day = 0.0
+	current_state = State.DOING_STUFF
 
 func get_visit_by_landmark(landmark : Landmark) -> int:
 	for visit in visits:
