42b756502be14618132b7ced06cebd711b95359a
dino460 <raphael.z.cali@gmail.com>
2025-08-12 10:39:32 -0300

    [0.3.24.0α] FEAT :: GLOBAL SETTINGS SCRIPT
    Created script to hold global settings, such as pixelation variables.
    
    Body:
    (+) [global_settings.gd] Created script to be responsible by the global variables and settings.
    (+) [Project Settings] Added global_settings.gd to autoload list as GlobalSettings.
    (+) [handle_pixelation.gd] _ready() gets pixelation data from GlobalSettings.
    (-) [handle_pixelation.gd] Removed @export from both vars.
    
    from: [0.3.23.0α]

---
+++ scripts/global_settings.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var minimum_pixelation    : int = 3
+var pixelation_correction : int = 3
--- scripts/handle_pixelation.gd
+++ scripts/handle_pixelation.gd
@@ -1,14 +1,17 @@
 extends SubViewportContainer
 
-@export var minimum_pixelation    : int = 5
-@export var pixelation_correction : int = 5
+var minimum_pixelation    : int
+var pixelation_correction : int
 
 
 func _ready() -> void:
+	minimum_pixelation = GlobalSettings.minimum_pixelation
+	pixelation_correction = GlobalSettings.pixelation_correction
+
 	get_viewport().connect("size_changed", _change_pixelation_by_resolution)
 	_change_pixelation_by_resolution()
 
 func _change_pixelation_by_resolution():
 	var current_res = mini(DisplayServer.window_get_size().x, DisplayServer.window_get_size().y)
 
-	stretch_shrink = max(minimum_pixelation, pixelation_correction * current_res / 1080)
+	stretch_shrink = int(max(minimum_pixelation, pixelation_correction * current_res / 1080.0))
