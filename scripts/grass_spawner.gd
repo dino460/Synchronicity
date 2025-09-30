3f2768f83c621ece325e83c2ed8d120a5b7ac98c
dino460 <raphael.z.cali@gmail.com>
2025-09-29 23:18:14 -0300

    [0.4.6.0α] FEAT :: GRASS SWAY/NEW OUTLINE
    Added grass movement to grass shader.
    Added new outline shader.
    
    Body:
    (+) Added randomized grass movement to grass shader for better looks.
    (+) Added new outline shader better suited for intended looks.
    (-) Disabled old outline shader.
    (-) Disabled adaptative player x-ray due to conflicts with new outline shader.
    
    Footer:
    Player x-ray still works, it just doesn't disable itself when not behind objects (can have artifacts because of weapon).
    
    from: [0.4.5.0α]

--- scripts/grass_spawner.gd
+++ scripts/grass_spawner.gd
@@ -10,6 +10,7 @@ var grass_mesh : MeshInstance3D
 func _ready():
 	# Create the multimesh.
 	# multimesh = MultiMesh.new()
+	multimesh.use_custom_data = true
 	# Set the format first.
 	multimesh.transform_format = MultiMesh.TRANSFORM_3D
 	# Then resize (otherwise, changing the format is not allowed).
@@ -19,8 +20,10 @@ func _ready():
 
 	multimesh.mesh = grass_mesh.mesh
 
+
 	# Set the transform of the instances.
 	for i in multimesh.visible_instance_count:
 		var position = Transform3D()
 		position = position.translated(Vector3(spawn_area_center.x + randf_range(-1.0, 1.0) * spawn_area_size / 2.0, spawn_area_center.y, spawn_area_center.z + randf_range(-1.0, 1.0) * spawn_area_size / 2.0))
 		multimesh.set_instance_transform(i, position)
+		multimesh.set_instance_custom_data(i, Color(randf(), randf(), randf(), randf()))
