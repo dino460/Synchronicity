e87a42321ab7651a7b0c9994118ddd14385a0ec4
dino460 <raphael.z.cali@gmail.com>
2026-05-10 11:23:05 -0300

    [0.5.5.13α] CHORE :: SMALL SHADER TWEAKS
    Small testing tweaks to toon shaders.
    
    from: [0.5.5.12α]

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -58,9 +58,6 @@ var last_direction_normalized     : Vector3 = Vector3.UP
 @export var debug_label : Label
 @export var label_anchor : Node3D
 
-@export var edge_shadow : MeshInstance3D
-@export var mask_viewport : SubViewport
-
 
 func _ready():
 	#weapon = $MeshPivot/Viking_Female/CharacterArmature/Skeleton3D/BoneAttachment3D.get_child(0)
@@ -99,7 +96,6 @@ func rotate_direction(direction_to_rotate : Vector3) -> Vector3:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta : float) -> void:
 	set_is_running()
-	edge_shadow.get_surface_override_material(0).set_shader_parameter("mask_texture", mask_viewport.get_texture())
 
 
 func _physics_process(delta : float) -> void:
